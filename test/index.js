a1b5891b3da8d5d1831e3b9f87988e76dd1b6f7f
Brandon Scott <brandonscott@gmail.com>
2025-12-01 14:55:07 -0500

    comment out and focus on recreating bug

--- test/index.js
+++ test/index.js
@@ -18,143 +18,152 @@ describe('Parsing names', function() {
 
     var names = [
         {
-            name: 'Mr. William R. Hearst, III',
+            name: 'Ms. Smith',
             result: {
-                salutation: 'Mr.',
-                firstName: 'William',
-                middleName: 'R.',
-                lastName: 'Hearst',
-                suffix: 'III',
-                fullName: 'Mr. William R. Hearst, III'
-            }
-        },{
-            name: 'William Randolph Hearst',
-            result: {
-                firstName: 'William',
-                lastName: 'Hearst',
-                middleName: 'Randolph',
-                fullName: 'William Randolph Hearst'
-            }
-        }, {
-            name: 'William R. De La Cruz',
-            result: {
-                firstName: 'William',
-                lastName: 'De La Cruz',
-                middleName: 'R.',
-                fullName: 'William R. De La Cruz'
-            }
-        }, {
-            name: 'Mr. William R. De La Cruz III',
-            result: {
-                salutation: 'Mr.',
-                firstName: 'William',
-                suffix: 'III',
-                lastName: 'De La Cruz',
-                middleName: 'R.',
-                fullName: 'Mr. William R. De La Cruz III'
-            }
-        }, {
-            name: 'William De Cruz',
-            result: {
-                firstName: 'William',
-                lastName: 'De Cruz',
-                fullName: 'William De Cruz'
-            }
-        }, {
-            name: 'William De La Cruz',
-            result: {
-                firstName: 'William',
-                lastName: 'De La Cruz',
-                fullName: 'William De La Cruz'
-            }
-        }, {
-			name: 'Mr. William R. Hugh Calum De La Cruz III',
-			result: {
-				salutation: 'Mr.',
-				firstName: 'William',
-				suffix: 'III',
-				lastName: 'De La Cruz',
-				middleName: 'R. Hugh Calum',
-				fullName: 'Mr. William R. Hugh Calum De La Cruz III'
-			}
-		}, {
-			name: 'William A. B. De La Cruz',
-			result: {
-				firstName: 'William',
-				middleName: 'A. B.',
-				lastName: 'De La Cruz',
-				fullName: 'William A. B. De La Cruz'
-			}
-		}, {
-			name: 'James Hugh Calum Laurie',
-			result: {
-				firstName: 'James',
-				middleName: 'Hugh Calum',
-				lastName: 'Laurie',
-				fullName: 'James Hugh Calum Laurie'
-			}
-		}, {
-			name: 'Kiefer William Frederick Dempsey George Rufus Sutherland',
-			result: {
-				firstName: 'Kiefer',
-				middleName: 'William Frederick Dempsey George Rufus',
-				lastName: 'Sutherland',
-				fullName: 'Kiefer William Frederick Dempsey George Rufus Sutherland'
-			}
-		}, {
-            name: 'William Hearst',
-            result: {
-                firstName: 'William',
-                lastName: 'Hearst',
-                fullName: 'William Hearst'
-            }
-        }, {
-            name: 'William Hearst Jr',
-            result: {
-                firstName: 'William',
-                suffix: 'Jr',
-                lastName: 'Hearst',
-                fullName: 'William Hearst Jr'
-            }
-        }, {
-            name: 'Hearst, William Jr',
-            result: {
-                firstName: 'William',
-                suffix: 'Jr',
-                lastName: 'Hearst',
-                fullName: 'William Hearst Jr'
-            }
-        }, {
-            name: 'Hearst, William Randolph',
-            result: {
-                firstName: 'William',
-                lastName: 'Hearst',
-                middleName: 'Randolph',
-                fullName: 'William Randolph Hearst'
-            }
-        }, {
-            name: 'Hearst, William, M.D.',
-            result: {
-                firstName: 'William',
-                suffix: 'M.D.',
-                lastName: 'Hearst',
-                fullName: 'William Hearst M.D.'
-            }
-        }, {
-            name: 'William',
-            result: {
-                firstName: 'William',
-                lastName: '',
-                fullName: 'William'
-            }
-        }, {
-            name: '',
-            result: {
+                salutation: 'Ms.',
                 firstName: '',
-                lastName: '',
-                fullName: ''
+                lastName: 'Smith',
+                fullName: 'Ms. Smith'
             }
-        }
+        },
+        // {
+        //     name: 'Mr. William R. Hearst, III',
+        //     result: {
+        //         salutation: 'Mr.',
+        //         firstName: 'William',
+        //         middleName: 'R.',
+        //         lastName: 'Hearst',
+        //         suffix: 'III',
+        //         fullName: 'Mr. William R. Hearst, III'
+        //     }
+        // },{
+        //     name: 'William Randolph Hearst',
+        //     result: {
+        //         firstName: 'William',
+        //         lastName: 'Hearst',
+        //         middleName: 'Randolph',
+        //         fullName: 'William Randolph Hearst'
+        //     }
+        // }, {
+        //     name: 'William R. De La Cruz',
+        //     result: {
+        //         firstName: 'William',
+        //         lastName: 'De La Cruz',
+        //         middleName: 'R.',
+        //         fullName: 'William R. De La Cruz'
+        //     }
+        // }, {
+        //     name: 'Mr. William R. De La Cruz III',
+        //     result: {
+        //         salutation: 'Mr.',
+        //         firstName: 'William',
+        //         suffix: 'III',
+        //         lastName: 'De La Cruz',
+        //         middleName: 'R.',
+        //         fullName: 'Mr. William R. De La Cruz III'
+        //     }
+        // }, {
+        //     name: 'William De Cruz',
+        //     result: {
+        //         firstName: 'William',
+        //         lastName: 'De Cruz',
+        //         fullName: 'William De Cruz'
+        //     }
+        // }, {
+        //     name: 'William De La Cruz',
+        //     result: {
+        //         firstName: 'William',
+        //         lastName: 'De La Cruz',
+        //         fullName: 'William De La Cruz'
+        //     }
+        // }, {
+		// 	name: 'Mr. William R. Hugh Calum De La Cruz III',
+		// 	result: {
+		// 		salutation: 'Mr.',
+		// 		firstName: 'William',
+		// 		suffix: 'III',
+		// 		lastName: 'De La Cruz',
+		// 		middleName: 'R. Hugh Calum',
+		// 		fullName: 'Mr. William R. Hugh Calum De La Cruz III'
+		// 	}
+		// }, {
+		// 	name: 'William A. B. De La Cruz',
+		// 	result: {
+		// 		firstName: 'William',
+		// 		middleName: 'A. B.',
+		// 		lastName: 'De La Cruz',
+		// 		fullName: 'William A. B. De La Cruz'
+		// 	}
+		// }, {
+		// 	name: 'James Hugh Calum Laurie',
+		// 	result: {
+		// 		firstName: 'James',
+		// 		middleName: 'Hugh Calum',
+		// 		lastName: 'Laurie',
+		// 		fullName: 'James Hugh Calum Laurie'
+		// 	}
+		// }, {
+		// 	name: 'Kiefer William Frederick Dempsey George Rufus Sutherland',
+		// 	result: {
+		// 		firstName: 'Kiefer',
+		// 		middleName: 'William Frederick Dempsey George Rufus',
+		// 		lastName: 'Sutherland',
+		// 		fullName: 'Kiefer William Frederick Dempsey George Rufus Sutherland'
+		// 	}
+		// }, {
+        //     name: 'William Hearst',
+        //     result: {
+        //         firstName: 'William',
+        //         lastName: 'Hearst',
+        //         fullName: 'William Hearst'
+        //     }
+        // }, {
+        //     name: 'William Hearst Jr',
+        //     result: {
+        //         firstName: 'William',
+        //         suffix: 'Jr',
+        //         lastName: 'Hearst',
+        //         fullName: 'William Hearst Jr'
+        //     }
+        // }, {
+        //     name: 'Hearst, William Jr',
+        //     result: {
+        //         firstName: 'William',
+        //         suffix: 'Jr',
+        //         lastName: 'Hearst',
+        //         fullName: 'William Hearst Jr'
+        //     }
+        // }, {
+        //     name: 'Hearst, William Randolph',
+        //     result: {
+        //         firstName: 'William',
+        //         lastName: 'Hearst',
+        //         middleName: 'Randolph',
+        //         fullName: 'William Randolph Hearst'
+        //     }
+        // }, {
+        //     name: 'Hearst, William, M.D.',
+        //     result: {
+        //         firstName: 'William',
+        //         suffix: 'M.D.',
+        //         lastName: 'Hearst',
+        //         fullName: 'William Hearst M.D.'
+        //     }
+        // }, {
+        //     name: 'William',
+        //     result: {
+        //         firstName: 'William',
+        //         lastName: '',
+        //         fullName: 'William'
+        //     }
+        // }, {
+        //     name: '',
+        //     result: {
+        //         firstName: '',
+        //         lastName: '',
+        //         fullName: ''
+        //     }
+        // }
     ];
 
     var fullest = [
@@ -207,27 +216,35 @@ describe('Parsing names', function() {
         }
     ];
 
-    it('Should parse all name attributes', function() {
+    // it('Should parse all name attributes', function() {
+    //     names.forEach(function(name, i, list){
+    //         var parsed = human.parseName(name.name);
+
+    //         expect(parsed).to.eql(name.result);
+    //     });
+    // });
+
+    it('Should parse salutation and last name only', function() {
         names.forEach(function(name, i, list){
             var parsed = human.parseName(name.name);
-
+        
             expect(parsed).to.eql(name.result);
         });
     });
 
-    it('Should parse fullest name', function() {
-        fullest.forEach(function(name, i, list){
-            var fullName = human.getFullestName(name.name);
+    // it('Should parse fullest name', function() {
+    //     fullest.forEach(function(name, i, list){
+    //         var fullName = human.getFullestName(name.name);
 
-            expect(fullName).to.eql(name.result.fullName);
-        });
-    });
+    //         expect(fullName).to.eql(name.result.fullName);
+    //     });
+    // });
 
-    it('Should parse all address attributes', function() {
-        addresses.forEach(function(address, i, list){
-            var parsed = human.parseAddress(address.address);
+    // it('Should parse all address attributes', function() {
+    //     addresses.forEach(function(address, i, list){
+    //         var parsed = human.parseAddress(address.address);
 
-            expect(parsed).to.eql(address.result);
-        });
-    });
+    //         expect(parsed).to.eql(address.result);
+    //     });
+    // });
 });
